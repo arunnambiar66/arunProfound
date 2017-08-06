fab88ce5cf8e4e4c852f5bdb38e1726eecf9b75a
arunnambiar66 <arunnambiar66@gmail.com>
2017-08-06 17:31:58 +0530

    updated

--- com/bridgelabz/util/Prime.java
+++ com/bridgelabz/util/Prime.java
@@ -3,25 +3,39 @@ public class Prime
 {
    public static void main(String args[])
    {
-      int i,number,m=0,flag=0;
+      int i,number,m,flag=0;
 
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter the number");
       number=sc.nextInt();
       m=number/2;
-      for(i=1;i<=number;i++)
-      {
-	if(number%i==0)
+
+	if(number<2)
+	{
+	System.out.println(number +"\tnot a prime number");
+	}
+
+	else
+	{
+
+       for(i=2;i<=m;i++)
+       {
+ 	if(number%i==0)
          {
             System.out.print(number+ "\tis not a prime number");
  	    flag=1;
             break;
          }
       }
+
       if(flag==0)
       {
          System.out.print(number+ "\tis a prime number");
       }
+  }
+
    }
 
 }
+  
+
