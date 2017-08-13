ea0ceac6b3858e52e8885d5075ce5ed72d630384
arunnambiar66 <arunnambiar66@gmail.com>
2017-08-13 10:57:05 +0530

    ignore file added

---
+++ com/bridgelabz/util/RuntimeError.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *  Compilation:  javac -d bin com/bridgelabz/util/RuntimeError.java
+ *  Execution:    java -cp bin com.bridgelabz.util.RuntimeError
+ *  
+ *  Purpose: To produce Runtime error
+ *  @author  Arun Nambiar
+ *  @version 1.0
+ *  @since   13-08-2017
+ *
+ ******************************************************************************/
+package com.bridgelabz.util;
+
+public class RuntimeError
+{
+   public static void display(String args[])
+   {
+      System.out.println("Hello");
+   }
+}
